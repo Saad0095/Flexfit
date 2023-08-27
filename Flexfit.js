128cac43e94684a109c5dbab896fd496663cf5f3
Saad0095 <109427990+Saad0095@users.noreply.github.com>
2023-08-27 23:25:25 +0500

    Create Flexfit.js

---
+++ Flexfit.js
@@ -0,0 +1,15 @@
+function ReadMore1(){
+    document.getElementById('para1').innerHTML +=  "to member satisfaction is evident through their constant upgrades and attention to detail. Apex Fitness Fusion deserves a solid five stars for their dedication to revolutionizing the fitness journey and providing an unparalleled fitness haven."
+
+    document.getElementById('read1').style.display = 'none'
+}
+
+function ReadMore2(){
+    document.getElementById('para2').innerHTML +=  "exceptional, providing members with a safe and comfortable environment. Whether you're a beginner or a seasoned athlete, FlexFit Gym is a five-star experience that guarantees you'll reach your fitness goals."
+
+    document.getElementById('read2').style.display = 'none'
+}
+
+function submit(){
+    alert("Your form has been submitted successfully")
+}
